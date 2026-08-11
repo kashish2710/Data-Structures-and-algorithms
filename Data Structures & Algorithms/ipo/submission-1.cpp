97cc7f7cd835389a105e8b5df811c48824919cba
kashish2710 <hsihsak2710@gmail.com>
2026-08-11 21:02:44 +0530

    Add: ipo - submission-1

---
+++ Data Structures & Algorithms/ipo/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>>vec;
+        int n=profits.size();
+        for(int i=0;i<n;i++){   vec.push_back({capital[i],profits[i]});
+}
+sort(vec.begin(),vec.end());
+priority_queue<int>pq;
+
+int i=0;
+while(k--){
+    while(i<n && vec[i].first<=w){
+        pq.push(vec[i].second);
+        i++;
+    }
+    if(pq.empty())break;
+    w+=pq.top();
+    pq.pop();
+
+}
+
+return w;
+    }
+};
