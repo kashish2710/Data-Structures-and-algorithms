5f2c32014f280feb96ece68689e5a2474fd72b91
kashish2710 <hsihsak2710@gmail.com>
2026-08-13 21:13:21 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.cpp
@@ -0,0 +1,64 @@
+class DSU{
+public:
+vector<int>rank;
+vector<int>parent;
+DSU(int n){
+rank.resize(n,1);
+parent.resize(n);
+for(int i=0;i<n;i++){
+    parent[i]=i;
+}
+}
+int findparent(int node){
+if(parent[node]==node)return node;
+return parent[node]=findparent(parent[node]);
+}
+
+void union_by_rank(int u,int v){
+    int ulp_u=findparent(u);
+    int ulp_v=findparent(v);
+    if(ulp_u==ulp_v)return;
+     if(rank[ulp_u]>rank[ulp_v]){
+        parent[ulp_v]=ulp_u;
+    }
+    else  if(rank[ulp_v]>rank[ulp_u]){
+        parent[ulp_u]=ulp_v;
+    }
+    else{
+        rank[ulp_u]++;
+        parent[ulp_v]=ulp_u;
+    }
+}
+};
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size();
+      DSU dsu(n);
+        vector<vector<int>>edges;
+        for(int i=0;i<points.size();i++){
+            for(int j=i+1;j<points.size();j++){
+                int x1=points[i][0];
+                int x2=points[j][0];
+                int y1=points[i][1];
+                int y2=points[j][1];
+             int dis = abs(y2-y1) + abs(x2-x1);
+                edges.push_back({dis,i,j});
+            }
+        }
+sort(edges.begin(),edges.end());
+int ans=0;
+for(int i=0;i<edges.size();i++){
+    int u=edges[i][1];
+    int v=edges[i][2];
+    int cost=edges[i][0];
+    if(dsu.findparent(u)!=dsu.findparent(v)){
+        ans+=cost;
+        dsu.union_by_rank(u,v);
+    }
+
+}
+return ans;
+    }
+};
