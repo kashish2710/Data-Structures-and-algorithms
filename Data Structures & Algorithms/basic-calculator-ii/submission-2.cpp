d2f2258fdb954d62febb6d6d062d9d35f26f4f05
kashish2710 <hsihsak2710@gmail.com>
2026-08-24 11:11:06 +0530

    Add: basic-calculator-ii - submission-2

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int>st;
+        int num=0;
+        char op='+';
+        int n=s.size();
+        for(int i=0;i<=n;i++){
+            char ch;
+if(i==n){
+    ch='+';
+}
+else ch=s[i];
+if(isdigit(ch)){
+    num=num*10+(ch-'0');
+}
+else if(ch!=' '){
+ if(op=='+'){
+st.push(num);
+}
+else if(op=='-'){
+   st.push(-num); 
+}
+else if(op=='*'){
+    int value=st.top();
+    st.pop();
+    st.push(num*value);
+}
+else if(op=='/'){
+     int value=st.top();
+    st.pop();
+    st.push(value/num);
+}
+op=ch;
+num=0;
+}
+        }
+        int ans=0;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
