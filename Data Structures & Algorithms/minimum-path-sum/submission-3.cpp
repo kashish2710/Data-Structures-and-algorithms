f17a2623ed55ef960eccb2ccb9552e197076f99f
kashish2710 <hsihsak2710@gmail.com>
2026-09-12 21:23:17 +0530

    Add: minimum-path-sum - submission-3

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int solve(int i,int j,vector<vector<int>>& grid,int m,int n,vector<vector<int>>& dp){
+    if(i<0 || i>m-1 || j<0 || j>n-1)return 1e9;
+    if(i==m-1 && j==n-1)return grid[i][j];
+    if(dp[i][j]!=-1)return dp[i][j];
+    int down=grid[i][j]+solve(i+1,j,grid,m,n,dp);
+    int right=grid[i][j]+solve(i,j+1,grid,m,n,dp);
+    return dp[i][j]=min(down,right);
+}
+    int minPathSum(vector<vector<int>>& grid) {
+       int m=grid.size();
+       int n=grid[0].size();
+       vector<vector<int>>dp(m,vector<int>(n,-1));
+return solve(0,0,grid,m,n,dp);
+    }
+};
